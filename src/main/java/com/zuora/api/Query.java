feba713b35038e9db71de37188b026c90d03192f
Pablo Diez <pablo@zauberlabs.com>
2012-08-29 14:48:39 -0300

    Adding cxf generated code to main folder

---
+++ src/main/java/com/zuora/api/Query.java
@@ -0,0 +1,80 @@
+/**
+ * Mule Zuora Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+
+package com.zuora.api;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import com.zuora.api.object.Dynamic;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="queryString" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "queryString"
+})
+@XmlRootElement(name = "query")
+public class Query
+    extends Dynamic
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 100L;
+    @XmlElement(required = true)
+    protected String queryString;
+
+    /**
+     * Gets the value of the queryString property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getQueryString() {
+        return queryString;
+    }
+
+    /**
+     * Sets the value of the queryString property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setQueryString(String value) {
+        this.queryString = value;
+    }
+
+}
